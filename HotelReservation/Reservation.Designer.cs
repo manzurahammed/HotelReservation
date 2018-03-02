eb06de50fe660803bebf4663fbfd409b41815d9a
manzurahammed <manzur.ahammed236@gmail>
2018-03-03 03:18:01 +0600

    some file added

---
+++ HotelReservation/Reservation.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace HotelReservation
+{
+    partial class Reservation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.room_list = new System.Windows.Forms.ComboBox();
+            this.customer_list = new System.Windows.Forms.ComboBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.book = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.total = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.hotelDataSet = new HotelReservation.hotelDataSet();
+            this.hotelDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.hotelDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hotelDataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Customer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select Room";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(278, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(177, 40);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Book Room";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // room_list
+            // 
+            this.room_list.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.room_list.FormattingEnabled = true;
+            this.room_list.IntegralHeight = false;
+            this.room_list.ItemHeight = 13;
+            this.room_list.Location = new System.Drawing.Point(212, 161);
+            this.room_list.Name = "room_list";
+            this.room_list.Size = new System.Drawing.Size(176, 21);
+            this.room_list.TabIndex = 3;
+            this.room_list.SelectedIndexChanged += new System.EventHandler(this.room_list_SelectedIndexChanged);
+            // 
+            // customer_list
+            // 
+            this.customer_list.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.customer_list.FormattingEnabled = true;
+            this.customer_list.ItemHeight = 13;
+            this.customer_list.Location = new System.Drawing.Point(212, 101);
+            this.customer_list.Name = "customer_list";
+            this.customer_list.Size = new System.Drawing.Size(176, 21);
+            this.customer_list.TabIndex = 4;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(88, 228);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(542, 165);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // book
+            // 
+            this.book.BackColor = System.Drawing.Color.Green;
+            this.book.ForeColor = System.Drawing.Color.White;
+            this.book.Location = new System.Drawing.Point(661, 355);
+            this.book.Name = "book";
+            this.book.Size = new System.Drawing.Size(81, 38);
+            this.book.TabIndex = 6;
+            this.book.Text = "Book";
+            this.book.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(658, 228);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Total";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(730, 261);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(115, 30);
+            this.textBox1.TabIndex = 8;
+            // 
+            // total
+            // 
+            this.total.AutoSize = true;
+            this.total.Location = new System.Drawing.Point(727, 228);
+            this.total.Name = "total";
+            this.total.Size = new System.Drawing.Size(22, 13);
+            this.total.TabIndex = 7;
+            this.total.Text = "0.0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(658, 270);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Advanced";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(658, 316);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(27, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Due";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(727, 316);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(22, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "0.0";
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Room Number";
+            this.columnHeader1.Width = 199;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Price";
+            this.columnHeader2.Width = 224;
+            // 
+            // hotelDataSet
+            // 
+            this.hotelDataSet.DataSetName = "hotelDataSet";
+            this.hotelDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // hotelDataSetBindingSource
+            // 
+            this.hotelDataSetBindingSource.DataSource = this.hotelDataSet;
+            this.hotelDataSetBindingSource.Position = 0;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "#";
+            this.columnHeader3.Width = 115;
+            // 
+            // Reservation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 421);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.total);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.book);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.customer_list);
+            this.Controls.Add(this.room_list);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Reservation";
+            this.Text = "Reservation";
+            this.Load += new System.EventHandler(this.Reservation_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.hotelDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hotelDataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox room_list;
+        private System.Windows.Forms.ComboBox customer_list;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button book;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label total;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private hotelDataSet hotelDataSet;
+        private System.Windows.Forms.BindingSource hotelDataSetBindingSource;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
---
+++ HotelReservation/Reservation.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace HotelReservation
+{
+    public partial class Reservation : Form
+    {
+        MySqlCommand Com = new MySqlCommand();
+        MySqlDataReader reader;
+        public Reservation()
+        {
+            InitializeComponent();
+            Com.Connection = Mycon.con;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Reservation_Load(object sender, EventArgs e)
+        {
+            Mycon.con.Close();
+            Mycon.con.Open();
+            customerlist();
+            roomlist();
+            room_list.SelectedIndex = 0;
+        }
+
+        public void customerlist()
+        {
+            Com.CommandText = "SELECT id,full_name FROM customer";
+            reader = Com.ExecuteReader();
+            customer_list.Items.Insert(0, "< Select Customer >");
+            
+            while (reader.Read())
+            {
+                //int.TryParse(t, out value);
+                customer_list.Items.AddRange("l");
+                customer_list.Items.Add("lol",reader["full_name"].ToString());
+            }
+            reader.Close();
+        }
+
+        public void roomlist()
+        {
+            Com.CommandText = "SELECT id,room_number FROM room WHERE book <> '" + 1 + "'";
+            reader = Com.ExecuteReader();
+            room_list.Items.Insert(0, "< Select Room >");
+            while (reader.Read())
+            {
+                room_list.Items.Add(reader["room_number"].ToString());
+            }
+            reader.Close();
+
+        }
+
+        private void room_list_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string room_number = "";
+            if (room_list.Text != "")
+            {
+                room_number = room_list.Text;
+            }
+            Com.CommandText = "SELECT * FROM room WHERE room_number = '" + room_number + "'";
+            reader = Com.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    if (!roomexist(reader["id"].ToString()))
+                    {
+                        ListViewItem list = new ListViewItem(reader["id"].ToString());
+                        list.SubItems.Add(reader["room_number"].ToString());
+                        list.SubItems.Add(reader["price"].ToString());
+                        listView1.Items.AddRange(new ListViewItem[] { list });
+                        string t = total.Text;
+                        int value;
+                        int.TryParse(t, out value);
+                        value = value + reader.GetInt32("price");
+                        total.Text = value.ToString();
+                    } else
+                    {
+                        MessageBox.Show("Room Already Add In List");
+                    }
+                    
+                }
+            }
+            reader.Close();
+        }
+
+        bool roomexist(string rnumber)
+        {
+            bool a = false;
+            for (int i = listView1.Items.Count - 1; i >= 0; i--)
+            {
+                var item = listView1.Items[i];
+                if (item.Text.ToLower().Contains(rnumber.ToLower()))
+                {
+                    a = true;
+                    break;
+                    
+                }
+            }
+            return a;
+        }
+    }
+}
